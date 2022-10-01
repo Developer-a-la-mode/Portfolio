ae587b4b77f2a5be70e775d08c5361fb63723169
Kevin Tonthat <tonthat.kevin@gmail.com>
2022-10-01 13:49:04 -0700

    project section

--- src/Components/Layout/Card.js
+++ src/Components/Layout/Card.js
@@ -4,127 +4,69 @@ import imageee from "../../img/lizard.jpg";
 
 const Card = () => {
   return (
-    <div className={classes.card__container}>
-      <div className={classes.card}>
-        <figure className={classes.card__image}>
-          <img src={imageee} />
-        </figure>
-        <div className={classes.card__content}>
-          <h2 className={classes.card__title}>Travel</h2>
-          <div className={classes.card__subtitle}>Travel Pro Tips</div>
-          <p className={classes.card__text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation.
-          </p>
+    <section className={classes.projects}>
+      <div className={classes.card__container}>
+        <div className={classes.card}>
+          <figure className={classes.card__image}>
+            <img src={imageee} />
+          </figure>
+          <div className={classes.card__content}>
+            <h2 className={classes.card__title}>Travel</h2>
+            <div className={classes.card__subtitle}>Travel Pro Tips</div>
+            <p className={classes.card__text}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation.
+            </p>
+          </div>
         </div>
-      </div>
 
-      <div className={classes.card}>
-        <figure className={classes.card__image}>
-          <img src={imageee} />
-        </figure>
-        <div className={classes.card__content}>
-          <h2 className={classes.card__title}>Travel</h2>
-          <div className={classes.card__subtitle}>Travel Pro Tips</div>
-          <p className={classes.card__text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation.
-          </p>
+        <div className={classes.card}>
+          <figure className={classes.card__image}>
+            <img src={imageee} />
+          </figure>
+          <div className={classes.card__content}>
+            <h2 className={classes.card__title}>Travel</h2>
+            <div className={classes.card__subtitle}>Travel Pro Tips</div>
+            <p className={classes.card__text}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation.
+            </p>
+          </div>
         </div>
-      </div>
 
-      <div className={classes.card}>
-        <figure className={classes.card__image}>
-          <img src={imageee} />
-        </figure>
-        <div className={classes.card__content}>
-          <h2 className={classes.card__title}>Travel</h2>
-          <div className={classes.card__subtitle}>Travel Pro Tips</div>
-          <p className={classes.card__text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation.
-          </p>
+        <div className={classes.card}>
+          <figure className={classes.card__image}>
+            <img src={imageee} />
+          </figure>
+          <div className={classes.card__content}>
+            <h2 className={classes.card__title}>Travel</h2>
+            <div className={classes.card__subtitle}>Travel Pro Tips</div>
+            <p className={classes.card__text}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation.
+            </p>
+          </div>
         </div>
-      </div>
 
-      <div className={classes.card}>
-        <figure className={classes.card__image}>
-          <img src={imageee} />
-        </figure>
-        <div className={classes.card__content}>
-          <h2 className={classes.card__title}>Travel</h2>
-          <div className={classes.card__subtitle}>Travel Pro Tips</div>
-          <p className={classes.card__text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation.
-          </p>
+        <div className={classes.card}>
+          <figure className={classes.card__image}>
+            <img src={imageee} />
+          </figure>
+          <div className={classes.card__content}>
+            <h2 className={classes.card__title}>Travel</h2>
+            <div className={classes.card__subtitle}>Travel Pro Tips</div>
+            <p className={classes.card__text}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation.
+            </p>
+          </div>
         </div>
       </div>
-
-      <div className={classes.card}>
-        <figure className={classes.card__image}>
-          <img src={imageee} />
-        </figure>
-        <div className={classes.card__content}>
-          <h2 className={classes.card__title}>Travel</h2>
-          <div className={classes.card__subtitle}>Travel Pro Tips</div>
-          <p className={classes.card__text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation.
-          </p>
-        </div>
-      </div>
-
-      <div className={classes.card}>
-        <figure className={classes.card__image}>
-          <img src={imageee} />
-        </figure>
-        <div className={classes.card__content}>
-          <h2 className={classes.card__title}>Travel</h2>
-          <div className={classes.card__subtitle}>Travel Pro Tips</div>
-          <p className={classes.card__text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation.
-          </p>
-        </div>
-      </div>
-
-      <div className={classes.card}>
-        <figure className={classes.card__image}>
-          <img src={imageee} />
-        </figure>
-        <div className={classes.card__content}>
-          <h2 className={classes.card__title}>Travel</h2>
-          <div className={classes.card__subtitle}>Travel Pro Tips</div>
-          <p className={classes.card__text}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-          ad minim veniam, quis nostrud exercitation.
-          </p>
-        </div>
-      </div>
-
-      <div className={classes.card}>
-        <figure className={classes.card__image}>
-          <img src={imageee} />
-        </figure>
-        <div className={classes.card__content}>
-          <h2 className={classes.card__title}>Travel</h2>
-          <div className={classes.card__subtitle}>Travel Pro Tips</div>
-          <p className={classes.card__text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation.
-          </p>
-        </div>
-      </div>
-    </div>
+    </section>
   );
 };
 
